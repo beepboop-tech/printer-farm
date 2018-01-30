fa2acece7309a630c7cd38c7c4c9411de3b965e4
Tom Evans <TomosAMEvans@gmail.com>
2018-01-30 16:40:13 +0000

    Added a basic printer class

---
+++ src/printers.py
@@ -0,0 +1,21 @@
+
+
+class Printer():
+
+    def status(self):
+        # TODO: Call the API to get the printer status
+        # TODO: Cheks that the button has been pressed?
+
+        return 'functional'
+
+    def make(self, job: PrintJob):
+        # TODO: Send the info from the PrintJob to the printer
+        pass
+
+    def power_down(self):
+        # TODO turn the bed plate off etc
+        pass
+
+    def power_up(self):
+        # TODO: printer set up
+        pass 
